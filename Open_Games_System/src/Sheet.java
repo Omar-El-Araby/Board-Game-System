f08e6284d42c18abedbbaa2644d242ec88700130
moustafa <moustafamamdouh2013@gmail.com>
2021-06-07 19:47:28 +0200

    Added HP Stat

--- Open_Games_System/src/Sheet.java
+++ Open_Games_System/src/Sheet.java
@@ -3,9 +3,9 @@ import org.json.JSONObject;
 import java.util.Hashtable;
 
 public interface Sheet {
-    String[] Main_Stats={"Str","Dex","Con","Int","Wis","char"};
+    String[] Main_Stats={"Str","Dex","Con","Int","Wis","HP"};
     Hashtable<String,Integer> Stats= new Hashtable<>();
-    Integer[] defaultValues={0,0,0,0,0};
+    Integer[] defaultValues={0,0,0,0,0,0};
     JSONObject writeData = new JSONObject();
     void read() throws Exception ;
     void store() throws Exception;
--- Open_Games_System/src/characterSheet.java
+++ Open_Games_System/src/characterSheet.java
@@ -21,14 +21,13 @@ public class characterSheet implements Sheet {
     public void store() {
         try {
             String currentFileLocation = "C:\\Users\\Hp\\Desktop\\OOP\\Code\\Open_Games_System\\charactersheet.json";
-
             JSONObject States = new JSONObject();
             File file = new File(currentFileLocation);
             if (!file.exists())
                 file.createNewFile();
             writeData.put("playerName", playerName);
             writeData.put("Level", playerLevel);
-            for (int i = 0; i < Main_Stats.length - 1; i++) {
+            for (int i = 0; i < Main_Stats.length ; i++) {
                 System.out.println(Main_Stats[i] + "  " + valueStoredForStat[i]);
                 States.put(Main_Stats[i], valueStoredForStat[i]);
             }
@@ -54,10 +53,9 @@ public class characterSheet implements Sheet {
     }
 
     public static void main(String[] args) throws Exception {
-       // characterSheet pl = new characterSheet("Dragonborn",new Integer[]{3,4,8,5,1},1);
-        characterSheet temp = new characterSheet();
-        temp.read();
-        temp.print();
+        characterSheet pl = new characterSheet("Dragonborn",new Integer[]{3,4,8,5,1,100},1);
+
+        //characterSheet temp = new characterSheet();
     }
     public void update()
     {
