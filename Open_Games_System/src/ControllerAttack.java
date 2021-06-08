2f9fbfa928a7003ec95d13de01174598e8f6261f
axhmed <ahmed_abdalla405@yahoo.com>
2021-06-08 22:44:35 +0200

    Polished the attack menu.

--- Open_Games_System/src/ControllerAttack.java
+++ Open_Games_System/src/ControllerAttack.java
@@ -3,34 +3,36 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
+import javax.swing.*;
+
 public class ControllerAttack {
     DNDToken sheet1;
     DNDToken sheet2;
     public Label tknName = new Label();
     public Label tknStats= new Label();
-    public int tknHP;
+    public int tknHP = 0;
     public Label tknTHP= new Label();
     public ImageView tknImg;
     public Label targetName= new Label();
     public Label targetStats= new Label();
-    public int targetHP;
+    public int targetHP = 0;
     public Label targetTHP= new Label();
     public ImageView targetImg = new ImageView();
     public void initAttacker(){
         sheet1 = Controller.gamer[Controller.selectedToken];
         tknName.setText(sheet1.getSheet().getPlayerName());
         tknStats.setText("Your stats: " + sheet1.getSheet().getStats().toString() + "\nAC: " + sheet1.getSheet().getAC());
-        tknImg = new ImageView(sheet1.getImage());
-        tknHP = sheet1.getSheet().getHP();
+        tknImg.setImage(sheet1.getImage());
+        if(tknHP == 0)tknHP = sheet1.getSheet().getHP();
         tknTHP.setText(sheet1.getSheet().getHP() + "/" + tknHP);
     }
     public void initTarget(){
         sheet2 = Controller.gamer[Controller.notSelected];
         targetName.setText(sheet2.getSheet().getPlayerName());
         targetStats.setText("Target stats: " + sheet2.getSheet().getStats().toString()+ "\nAC: " + sheet2.getSheet().getAC());
-        targetHP = sheet2.getSheet().getHP();
+        if(targetHP == 0)targetHP = sheet2.getSheet().getHP();
         targetTHP.setText(sheet2.getSheet().getHP() + "/" + targetHP);
-        targetImg = new ImageView(sheet2.getImage());
+        targetImg.setImage(sheet2.getImage());
     }
     @FXML
     public void initialize() {
@@ -38,13 +40,16 @@ public class ControllerAttack {
     }
     @FXML
     public void Attack() {
-        Combat.Weapon.Attack(sheet1,sheet2);
-        init();
+        JOptionPane.showMessageDialog(null,
+                Combat.Weapon.Attack(sheet1,sheet2),
+                "Your Roll",
+                JOptionPane.INFORMATION_MESSAGE);
+        initialize();
     }
     @FXML
     public void Switch() {
         Controller.DNDSwitchTokens();
-        init();
+        initialize();
     }
 
     public void init() {
