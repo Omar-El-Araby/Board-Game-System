7d71fc9f763e0922b4c64fd390a1c268c7fffc1d
moustafa <moustafamamdouh2013@gmail.com>
2021-06-08 12:19:21 +0200

    Fixed the register and login menus

--- Open_Games_System/src/ControllerLogin.java
+++ Open_Games_System/src/ControllerLogin.java
@@ -7,16 +7,22 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ControllerLogin {
     public TextField usr;
     public TextField pass;
     public CheckBox rememberme;
     public Stage register = new Stage();
-
+    private JSONObject reader = new JSONObject();
     public void initialize() throws IOException {
         register.initStyle(StageStyle.UNDECORATED);
         register.setResizable(false);
@@ -30,19 +36,46 @@ public class ControllerLogin {
     }
 
     public void login(ActionEvent actionEvent) {
-        if(usr.getText().isEmpty() || pass.getText().isEmpty()) {
+        boolean flag=false;
+        try{
+            String File = new String(Files.readAllBytes(Paths.get("userCreds.json")));
+            reader = new JSONObject(File);
+        }catch (IOException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+        JSONArray usernames=reader.getJSONArray("Usernames");
+        JSONArray passwords=reader.getJSONArray("Passwords");
+        for(int i=0;i<usernames.length();i++)
+        {
+            if(usr.getText().equals(usernames.get(i))&&pass.getText().equals(passwords.get(i)))
+            {
+                JOptionPane.showMessageDialog(null,
+                        "Login Successful",
+                        "Login",
+                        JOptionPane.INFORMATION_MESSAGE);
+                User.setCurrentUser(usr.getText());
+                Main.controller.refreshUser();
+                Main.controller.login.hide();
+                flag=true;
+            }
+            if(usr.getText().equals(reader.getString("Admin"))&&pass.getText().equals(reader.getString("Admin_Passwords")))
+            {
+                JOptionPane.showMessageDialog(null,
+                        "Admin Login Successful",
+                        "Login",
+                        JOptionPane.INFORMATION_MESSAGE);
+                User.setCurrentUser(usr.getText());
+                Main.controller.refreshUser();
+                Main.controller.login.hide();
+                flag=true;
+            }
+        }
+        if(!flag)
             JOptionPane.showMessageDialog(null,
-                    "Please enter Username and Password",
-                    "Invalid login",
+                    "Login failed check password or username",
+                    "Login",
                     JOptionPane.INFORMATION_MESSAGE);
-        }
-        else {
-            User.setCurrentUser(usr.getText());
-            if (rememberme.isSelected()) User.setRememberme(true);
-            else User.setRememberme(false);
-            Main.controller.refreshUser();
-            Main.controller.login.hide();
-        }
     }
 
     private double xOffset = 0, yOffset = 0;    //For dragging the window.
--- Open_Games_System/src/ControllerRegister.java
+++ Open_Games_System/src/ControllerRegister.java
@@ -1,11 +1,22 @@
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ControllerRegister {
     public TextField usr;
     public TextField pass;
-
+    private final String currentFileLocation = "userCreds.json";
+    private final File file = new File(currentFileLocation);
     private double xOffset = 0, yOffset = 0;    //For dragging the window.
 
     public void bgOnMousePressed(MouseEvent mouseEvent) {
@@ -22,5 +33,26 @@ public class ControllerRegister {
     }
 
     public void register(ActionEvent actionEvent) {
+        String username = usr.getText();
+        String password = pass.getText();
+        try {
+            String File = new String(Files.readAllBytes(Paths.get("userCreds.json")));
+            JSONObject reader = new JSONObject(File);
+            JSONArray usernames= reader.getJSONArray("Usernames");
+            JSONArray passwords= reader.getJSONArray("Passwords");
+            usernames.put(username);
+            passwords.put(password);
+            System.out.println(reader.toString());
+            PrintWriter wrt = new PrintWriter(file);
+            wrt.write(reader.toString()+" ");
+            wrt.close();
+        }catch (IOException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+        JOptionPane.showMessageDialog(null,
+                "Registered Succesfully",
+                "Register",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 }
